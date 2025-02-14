7b441066bed5f97df5f1d99fdd943953dc8eac45
amira <moriffamira@gmail.com>
2025-02-14 15:10:22 +0800

    delete stuff

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import theme from './Theme';
-import { ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
   </React.StrictMode>
   
 );
